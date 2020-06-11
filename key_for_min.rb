d6a8c0e0a31164fa89f09324901e320d42e4fac1
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-11 15:28:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  key = nil
+  value = nil
   name_hash.each do |key, value|
-    if key == value.last.to.h
+    if key == value
       key
     end
   end
